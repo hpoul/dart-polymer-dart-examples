9a3b81003c221314b365f3f9ceffadc710a05d16
Herbert Poul <herbert@poul.at>
2013-09-18 09:17:23 +0200

    simple test for mouse events with embedded components

--- web/custom_element_with_style/index.dart
+++ web/custom_element_with_style/index.dart
@@ -1,10 +1,15 @@
 import 'package:polymer/polymer.dart';
+import 'dart:html';
 
 // XXX: Must always have a class for every custom element.
 // See https://code.google.com/p/dart/issues/detail?id=12254
 
 @CustomTag('my-element')
-class MyElement extends PolymerElement { }
+class MyElement extends PolymerElement {
+  void moveEvent(Event e, var detail, Node target) => print('moveEvent ${new DateTime.now()}');
+  void downEvent(Event e, var detail, Node target) => print('downEvent ${new DateTime.now()}');
+  void upEvent(Event e, var detail, Node target) => print('upEvent ${new DateTime.now()}');
+}
 
 @CustomTag('my-child')
 class MyChild extends PolymerElement { }
